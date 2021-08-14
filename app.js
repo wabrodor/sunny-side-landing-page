e15ff6555fbcca3216d4a251715b5d6b7062b44e
unknown <wabrodor@gmail.com>
2021-08-14 20:34:09 +0300

    final update

---
+++ app.js
@@ -0,0 +1,7 @@
+const toggle = document.querySelector(".toggle")
+const list = document.querySelector(".nav-links")
+
+toggle.addEventListener("click",  () =>{
+list.classList.toggle("active")
+})
+
